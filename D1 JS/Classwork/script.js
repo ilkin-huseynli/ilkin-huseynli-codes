79af22ba7c65f1450c6b91370c952ec1907ba806
ilkin-huseynli <ihuseynli403@gmail.com>
2026-06-15 17:48:48 +0400

    js console and variables homework

---
+++ D1 JS/Classwork/script.js
@@ -0,0 +1,20 @@
+var city = "Baki";
+let country = "Azerbaycan";
+const year = 2026;
+
+alert(city);
+alert(country);
+alert(year);
+
+const salam = "salam";
+console.log(salam);
+
+let fruit = "Alma";
+document.write("My Fruit " +fruit);
+
+var color = "qirmizi";
+var color = "mavi";
+console.log(color);
+
+let name = "Ilkin";
+alert("Salam," +name+ "Necesen?");
