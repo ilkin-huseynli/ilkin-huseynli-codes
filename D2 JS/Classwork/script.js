9d7c4787171fa0f4e014b8ca8fefcb73548366c4
ilkin-huseynli <ihuseynli403@gmail.com>
2026-06-18 17:34:00 +0400

    D2 JS Classwork

---
+++ D2 JS/Classwork/script.js
@@ -0,0 +1,21 @@
+let ad = prompt("Adinizi daxil edin:");
+console.log("Salam, " + ad + "!");
+
+let a = +prompt("Birinci ədədi daxil edin:");
+let b = +prompt("İkinci ədədi daxil edin:");
+console.log("Cəm: " + (a + b));
+
+let dogumIli = +prompt("Doğum ilinizi daxil edin:");
+let indikiIl = 2026;
+let yas = indikiIl - dogumIli;
+console.log("Yasiniz: " + yas);
+
+let say = 50;
+let metn = String(say);
+console.log(metn);
+
+let x = +prompt("Birinci ədədi daxil edin:");
+let y = +prompt("İkinci ədədi daxil edin:");
+let z = +prompt("Üçüncü ədədi daxil edin:");
+let orta = (x + y + z) / 3;
+console.log(+orta)
