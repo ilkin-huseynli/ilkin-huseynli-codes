48cfc20dd355b364eb2ee738664def4a572d00cf
ilkin-huseynli <ihuseynli403@gmail.com>
2026-06-27 22:26:35 +0400

    D4 JS Homework

---
+++ D4 JS/Homework/script.js
@@ -0,0 +1,21 @@
+let number1 = 5;
+number1+=4;
+number1*=4;
+number1**=2;
+++number1;
+number1+=8;
+number1*=9;
+number1**=2;
+++number1;
+console.log(number1)
+
+let number2 = 10;
+number2+=9;
+number2*=9;
+number2-=9;
+number2/=3;
+number2+=8;
+number2*=4;
+number2-=2;
+number2/=3;
+console.log(number2)
