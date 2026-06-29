7830a687683c62b8ba25f75502a44bee8ee7dd7c
ilkin-huseynli <ihuseynli403@gmail.com>
2026-06-29 17:34:48 +0400

    D5 JS Classwork

---
+++ D5 JS/Classwork/script.js
@@ -0,0 +1,9 @@
+let qiymet1 = +prompt("Birinci qiymət:");
+let qiymet2 = +prompt("İkinci qiymət:");
+
+let cem = qiymet1 + qiymet2;
+console.log("Cəm:", cem);
+
+let qiymet = +prompt("İmtahan qiymətini daxil et:");
+let netice = qiymet >= 50? "İmtahandan keçdin": "Kəsildin";
+console.log(netice);
