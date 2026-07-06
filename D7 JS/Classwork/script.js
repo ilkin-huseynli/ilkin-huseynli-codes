a076226142cfea34ceceb967872974601e53366f
ilkin-huseynli <ihuseynli403@gmail.com>
2026-07-06 13:53:59 +0400

    D7 JS Classwork

---
+++ D7 JS/Classwork/script.js
@@ -0,0 +1,12 @@
+function SalamDunya() {
+  console.log("Salam dünya!");
+}
+
+SalamDunya();
+
+function salamla() {
+  let ad = prompt("Adınızı daxil edin:");
+  console.log("Salam, " +ad+ "!");
+}
+
+salamla();
