a2ffd1dbb0cba4822b39ba24961082e31383376e
ilkin-huseynli <ihuseynli403@gmail.com>
2026-07-09 00:08:23 +0400

    D8 JS Classwork

---
+++ D8 JS/Classwork/script.js
@@ -0,0 +1,7 @@
+let text1 = "   salam, proqramçı dostlar!   ";
+let text1_bosluqsuz = text1.trim().toUpperCase();
+console.log(text1_bosluqsuz);
+
+let text2 = "nicat"; 
+let text2_duzeldilmis = text2.charAt(0) + text2.toUpperCase() + text2.slice(1);
+console.log(text2_duzeldilmis);
