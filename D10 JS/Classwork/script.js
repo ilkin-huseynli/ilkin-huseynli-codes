680db57f61c278a2950789003277361773cb724c
ilkin-huseynli <ihuseynli403@gmail.com>
2026-07-15 13:57:54 +0400

    D10 JS Claswork

---
+++ D10 JS/Classwork/script.js
@@ -0,0 +1,12 @@
+let eliPoints = 120;
+let leylaPoints = 155;
+let ferq = Math.abs(eliPoints - leylaPoints);
+console.log(ferq);
+
+let qiymet = 4.6;
+let yuxari = Math.ceil(qiymet);
+console.log(yuxari);
+let asagi = Math.floor(qiymet);
+console.log(asagi);
+let riyazi = Math.round(qiymet);
+console.log(riyazi);
