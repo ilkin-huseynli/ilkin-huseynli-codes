4b6fb45dd8724fa31c6ee8e569b0a4f9e8147be7
ilkin-huseynli <ihuseynli403@gmail.com>
2026-07-15 22:49:17 +0400

    D10 JS Homework

---
+++ D10 JS/Homework/script.js
@@ -0,0 +1,11 @@
+// 1
+let suretler = [12, 15, 9, 14, 11, 20];
+let enKicik = Math.min(...suretler);
+let enBoyuk = Math.max(...suretler);
+console.log("Massiv:", suretler);
+console.log("Ən kiçik saniyə:", enKicik);
+console.log("Ən böyük saniyə:", enBoyuk);
+
+// 2
+let zer = Math.floor(Math.random() * 6) + 1;
+console.log("Zər atıldı:", zer);
