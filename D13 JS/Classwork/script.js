b4a417fe6a38b65c41af0a144e7186c20455d7b0
ilkin-huseynli <ihuseynli403@gmail.com>
2026-07-22 14:03:05 +0400

    D13 JS Classwork

---
+++ D13 JS/Classwork/script.js
@@ -0,0 +1,6 @@
+let qiymetler = [65, 80, 45, 92, 58];
+let böyükdür = qiymətlər.every(qiymət => qiymət > 50);
+console.log(böyükdür);
+
+let ənAzıBiriBöyükdür = qiymətlər.some(qiymət => qiymət >= 90);
+console.log(ənAzıBiriBöyükdər);
