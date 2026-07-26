cd3f13bf89b1f4980dd50ec8ee6a438c13b8a387
ilkin-huseynli <ihuseynli403@gmail.com>
2026-07-26 13:04:16 +0400

    D14 JS Classwork

---
+++ D14 JS/Classwork/script.js
@@ -0,0 +1,16 @@
+for (let i = 1; i < 50; i++) {
+    console.log(i)
+}
+
+let index = 0;
+while (index <= 20) {
+    console.log(index);
+    index++;
+}
+
+let ededler = [5, 12, 8, 20, 3, 15];
+let cem = 0;
+for (let i = 0; i < ededler.length; i++) {
+  cem += ededler[i];
+}
+console.log("Cem: " + cem);
